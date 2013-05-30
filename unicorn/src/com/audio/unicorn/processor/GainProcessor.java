76901966c531a0d49e7397832cf32950e6bdc7c8
jsaund <jag.saund@alumni.utoronto.ca>
2013-05-30 16:47:13 -0700

    Audio Gain Processor

---
+++ unicorn/src/com/audio/unicorn/processor/GainProcessor.java
@@ -0,0 +1,32 @@
+package com.audio.unicorn.processor;
+
+/**
+ * Created by jsaund on 5/30/13.
+ */
+public class GainProcessor implements UnicornAudioProcessor {
+    public static final float GAIN_MIN = 0.0f;
+    public static final float GAIN_MAX = 1.0f;
+
+    private float mGain = GAIN_MAX;
+
+    public GainProcessor() {
+
+    }
+
+    public void setGain(float gain) {
+        mGain = gain;
+    }
+
+    public synchronized float getGain() {
+        return mGain;
+    }
+
+    @Override
+    public boolean processAudio(byte[] buffer) {
+        for (int i=0; i<buffer.length; i++) {
+            buffer[i] = (byte) (buffer[i] * mGain);
+        }
+
+        return true;
+    }
+}
