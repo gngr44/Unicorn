1194e6e33a6087ec4bf25cfc74fa50368e4ed64a
gngr44 <gngr44@gmail.com>
2013-05-30 21:06:02 -0700

    Set bitmap to the slot and rotate it while playing

--- unicorn/src/com/audio/unicorn/fragment/TrackControlFragment.java
+++ unicorn/src/com/audio/unicorn/fragment/TrackControlFragment.java
@@ -1,24 +1,36 @@
 package com.audio.unicorn.fragment;
 
+import java.io.FileNotFoundException;
+
 import com.audio.unicorn.R;
 import com.audio.unicorn.engine.UnicornAudioEngine;
 import com.audio.unicorn.engine.exception.InvalidMediaTypeException;
+import com.audio.unicorn.media.AlbumArtProvider;
 import com.audio.unicorn.media.Track;
 import com.audio.unicorn.view.TrackControlView;
 import com.audio.unicorn.view.TrackControlView.OnDropListener;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.app.Fragment;
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.animation.CycleInterpolator;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
 
 public class TrackControlFragment extends Fragment implements OnDropListener, OnClickListener {
 
     private TrackControlView mControlView;
     private UnicornAudioEngine mAudioEngine;
+    private ObjectAnimator mSlotAnimation;
 
     public static TrackControlFragment newInstance() {
         return new TrackControlFragment();
@@ -36,6 +48,7 @@ public class TrackControlFragment extends Fragment implements OnDropListener, On
     @Override
     public void onDrop(TrackControlView view, Track track) {
         Log.d("TEST", "drop: " + track.getTrackId() + " albumId: " + track.getAlbumId() + " title: " + track.getTitle());
+        new ImageLoadThread(getActivity(), mControlView.getTrackSlotView(), track.getAlbumId()).start();
         String filePath = track.getFilePath();
         if (mAudioEngine != null) {
             mAudioEngine.destroy();
@@ -47,6 +60,13 @@ public class TrackControlFragment extends Fragment implements OnDropListener, On
     private void startEngine() {
         try {
             mAudioEngine.start();
+            if (mSlotAnimation == null) {
+                mSlotAnimation = ObjectAnimator.ofFloat(mControlView.getTrackSlotView(), "rotation", 360);
+                mSlotAnimation.setDuration(2000);
+                mSlotAnimation.setInterpolator(new LinearInterpolator());
+                mSlotAnimation.setRepeatCount(ObjectAnimator.INFINITE);
+            }
+            mSlotAnimation.start();
         } catch (InvalidMediaTypeException e) {
             Log.e("TEST", "", e);
         }
@@ -56,11 +76,49 @@ public class TrackControlFragment extends Fragment implements OnDropListener, On
     public void onClick(View v) {
         if (mAudioEngine != null) {
             if (mAudioEngine.isPlaying()) {
-                mAudioEngine.stop();
+                stopEngine();
             } else {
                 startEngine();
             }
 
         }
+    }
+
+    private void stopEngine() {
+        mAudioEngine.stop();
+        mSlotAnimation.cancel();
+    }
+
+    private class ImageLoadThread extends Thread {
+
+        private long mAlbumId;
+        private FileNotFoundException mException;
+        private Context mContext;
+        private Handler mHandler;
+        private ImageView mImageView;
+
+        public ImageLoadThread(Context context, ImageView imageView, long albumId) {
+            mContext = context;
+            mImageView = imageView;
+            mAlbumId = albumId;
+            mHandler = new Handler();
+        }
+
+        @Override
+        public void run() {
+            try {
+                final Bitmap bitmap = new AlbumArtProvider().getCircularAlbumArtwork(mContext, mAlbumId);
+                mHandler.post(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        mImageView.setImageBitmap(bitmap);
+                    }
+                });
+            } catch (FileNotFoundException e) {
+                mException = e;
+            }
+        }
+
     }
 }
